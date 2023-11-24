0131fc12d3a3414dee2a375e20df7d41fe2e82d9
mubinbin <dinobunmu@gmail.com>
2023-11-23 21:41:52 -0600

    Initial Commit

---
+++ MyFirstIosProject/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MyFirstIosProject
+//
+//  Created by Binbin Mu on 11/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("yeah")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MyFirstIosProject/MyFirstIosProjectApp.swift
@@ -0,0 +1,18 @@
+//
+//  MyFirstIosProjectApp.swift
+//  MyFirstIosProject
+//
+//  Created by Binbin Mu on 11/23/23.
+//
+
+import SwiftUI
+
+@main
+struct MyFirstIosProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
