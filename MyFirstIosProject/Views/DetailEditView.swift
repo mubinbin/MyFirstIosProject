626a6e891bdad7d4910b2bb12de99d4efcae4f57
mubinbin <dinobunmu@gmail.com>
2023-11-25 20:31:43 -0600

    Displayed attendees in the edit view

--- MyFirstIosProject/Views/DetailEditView.swift
+++ MyFirstIosProject/Views/DetailEditView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct DetailEditView: View {
     @State private var newScurm = DailyScrum.emtpyScrum
+    @State private var newAttendeeName = ""
     
     var body: some View {
         Form {
@@ -21,6 +22,29 @@ struct DetailEditView: View {
                     }
                     Spacer()
                     Text("\(newScurm.lengthInMinutes) minutes")
+                }
+            }
+            
+            Section(header: Text("Attendees")) {
+                ForEach(newScurm.attendees) { attendee in
+                    Text(attendee.name)
+                }
+                .onDelete(perform: { indexSet in
+                    newScurm.attendees.remove(atOffsets: indexSet)
+                })
+                
+                HStack {
+                    TextField("New Attendee", text: $newAttendeeName)
+                    Button(action: {
+                        withAnimation {
+                            let attendee = DailyScrum.Attendee(name: newAttendeeName)
+                            newScurm.attendees.append(attendee)
+                            newAttendeeName = ""
+                        }
+                    }) {
+                        Image(systemName: "plus.circle.fill")
+                    }
+                    .disabled(newAttendeeName.isEmpty)
                 }
             }
         }
