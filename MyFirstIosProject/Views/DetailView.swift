080de2e3df2e4b0008b241691a3546a43a9aa704
mubinbin <dinobunmu@gmail.com>
2023-11-25 18:43:52 -0600

    Added visual components to the detail view

---
+++ MyFirstIosProject/Views/DetailView.swift
@@ -0,0 +1,18 @@
+//
+//  DetailView.swift
+//  MyFirstIosProject
+//
+//  Created by Binbin Mu on 11/25/23.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    DetailView()
+}
