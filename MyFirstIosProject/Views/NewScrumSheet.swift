f2ee21f62265f6da8a42a60008ed87284a5233f6
mubinbin <dinobunmu@gmail.com>
2023-12-08 18:45:27 -0600

    used the edit view to create a new scrum and refactored edit view

---
+++ MyFirstIosProject/Views/NewScrumSheet.swift
@@ -0,0 +1,38 @@
+//
+//  NewScrumSheet.swift
+//  MyFirstIosProject
+//
+//  Created by Binbin Mu on 12/8/23.
+//
+
+import SwiftUI
+
+struct NewScrumSheet: View {
+    
+    @State private var newScrum = DailyScrum.emtpyScrum
+    @Binding var scrums: [DailyScrum]
+    @Binding var isPresentingNewScrumView: Bool
+    
+    var body: some View {
+        NavigationStack {
+            DetailEditView(newScurm: $newScrum)
+                .toolbar {
+                    ToolbarItem(placement: .cancellationAction) {
+                        Button("Dismiss") {
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                    ToolbarItem(placement: .confirmationAction) {
+                        Button("Add") {
+                            scrums.append(newScrum)
+                            isPresentingNewScrumView = false
+                        }
+                    }
+                }
+        }
+    }
+}
+
+#Preview {
+    NewScrumSheet(scrums: .constant(DailyScrum.sampleData), isPresentingNewScrumView: .constant(true))
+}
