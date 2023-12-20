1c4886835febc77ba0eeed8ce7f4ec942845fbba
mubinbin <dinobunmu@gmail.com>
2023-12-20 15:29:10 -0600

    Save data in inactive state

--- MyFirstIosProject/MyFirstIosProjectApp.swift
+++ MyFirstIosProject/MyFirstIosProjectApp.swift
@@ -13,14 +13,22 @@ struct MyFirstIosProjectApp: App {
     
     var body: some Scene {
         WindowGroup {
-            ScrumsView(scrums: $store.scrums)
-                .task {
-                    do{
-                        try await store.load()
+            ScrumsView(scrums: $store.scrums) {
+                Task {
+                    do {
+                        try await store.save(scrums: store.scrums)
                     } catch {
                         fatalError(error.localizedDescription)
                     }
                 }
+            }
+            .task {
+                do{
+                    try await store.load()
+                } catch {
+                    fatalError(error.localizedDescription)
+                }
+            }
         }
     }
 }
